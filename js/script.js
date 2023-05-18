4bd737a522b3096c21ca479f229a0fc407296269
JMA <juanmaarellano10@gmail.com>
2023-05-18 16:03:26 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+const nombre = "Nombre";
+const apellido = "Apellido";
+console.log(nombre +" "+ apellido);
+
+const num1 = 10;
+const num2 = 8;
+const suma = num1 + num2;
+console.log(suma);
+
+const num3 = suma + 6;
+console.log(num3);
+
+/*Prueba de suma de variables de distinto tipo*/
+const num4 = "dos";
+const suma2 = num2 + num4;
+console.log(suma2)
